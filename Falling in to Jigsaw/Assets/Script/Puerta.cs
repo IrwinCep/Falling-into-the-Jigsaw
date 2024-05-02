24cdebf1a7c68362aa773724fe18828fe057316f
IrwinCep <142370646+IrwinCep@users.noreply.github.com>
2024-05-02 00:18:38 -0600

    Mecanica bien

--- Falling in to Jigsaw/Assets/Script/Puerta.cs
+++ Falling in to Jigsaw/Assets/Script/Puerta.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Puerta : MonoBehaviour
 {
-    public Animator animator;
+    private Animator animator;
     void Start()
     {
         animator = GetComponent<Animator>();
